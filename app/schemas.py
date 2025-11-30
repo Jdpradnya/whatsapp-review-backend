355286d60a7da984907171e316f97013a8b7b324
Jdpradnya <132432665+Jdpradnya@users.noreply.github.com>
2025-11-30 14:51:45 +0530

    Add Pydantic schemas

---
+++ app/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class ReviewBase(BaseModel):
+    contact_number: str
+    user_name: str
+    product_name: str
+    product_review: str
+
+class ReviewCreate(ReviewBase):
+    pass
+
+class ReviewResponse(ReviewBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
